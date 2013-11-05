04c90ddec8c230dfdcd7a76ab1d0460385f947c6
pathfinder <chm2920@gmail.com>
2013-11-05 22:19:32 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nx256::Application.config.secret_token = 'd0f9ba726f63d183adfc41bac78ae5b3994efb435e282c59e89c5b4221fb53147e09d0ef8cc8f1d4bd015d16510bda838433f5b73c53ef0f7215091539285571'
