53039be7bf7a71a1fee3e01f2e4327b35c361127
pathfinder <chm2920@gmail.com>
2013-11-08 00:37:13 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Nx256::Application.routes.draw do
   
+  match "products" => "start#products"
+  match "e-commerce" => "start#e-commerce"
+  match "services" => "start#services"
+  match "about_us" => "start#about_us"
   
   
   root :to => 'start#index'
